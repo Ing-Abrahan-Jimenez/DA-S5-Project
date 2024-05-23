5a12557d683d322efa9d8721c5a2f681c07d95cd
Ing-Abrahan-Jimenez <169616802+Ing-Abrahan-Jimenez@users.noreply.github.com>
2024-05-22 22:36:21 -0600

    Varias gráficas añadidas, primer intento de revisión.

--- app.py
+++ app.py
@@ -2,17 +2,57 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 data_vehicles = pd.read_csv(
-    r"D:\Cursos\Data analyst Triple ten\Proyectos triple ten\Sprint 5\vehicles_us.csv")
+    r"D:\Cursos\Data analyst Triple ten\Proyectos triple ten\Sprint 5\DA-S5-Project\vehicles_us.csv")
 
 st.title('Enterprise cars stock')
 st.header('Vehicels in stock analyse')
-st.write('Check out wich kind of car you can buy in the store, in the next graphs you will see a big view of all our ptoducts.')
+st.write('Check out wich car fits to your wallet, in the next graphs you will see a big view of all our ptoducts.')
 st.subheader('Prices')
 st.write('Click on the next button to know all our prices.')
 price_hst_butt = st.button('Take a look to our prices!')
 if price_hst_butt:
     st.write('These are our different prices!')
-    price_hst = px.histogram(data_vehicles, x="price")
+    st.write(
+        'Each point is a different price for the model that you are interesed in.')
+    price_hst = px.scatter(data_vehicles, y="price", x="model")
     st.plotly_chart(price_hst, use_container_width=True)
-
-st.header("this web page is not complete...")
+st.subheader('Odometer x Price')
+st.write('The next button will show you something speciall...')
+od_pr_butt = st.button("See what I'm talking about")
+if od_pr_butt:
+    od_pr = px.scatter(data_vehicles, x='odometer', y='price')
+    st.plotly_chart(od_pr, use_container_widht=True)
+    st.write('Most of our cars has low milles in the odometer :sunglasses:')
+st.subheader('Car conditions')
+st.write('Are you loking for the best? Take a look to the car conditions & their prices!')
+good = st.checkbox('Good')
+good_cond = data_vehicles[data_vehicles['condition'] == 'good']
+like_new = st.checkbox('Like new')
+l_n_cond = data_vehicles[data_vehicles['condition'] == 'like new']
+excellent = st.checkbox('Excelent')
+ex_cond = data_vehicles[data_vehicles['condition'] == 'excellent']
+new = st.checkbox('New')
+new_cond = data_vehicles[data_vehicles['condition'] == 'new']
+fair = st.checkbox('Fair')
+fair_cond = data_vehicles[data_vehicles['condition'] == 'cond']
+salvage = st.checkbox('Salvage')
+sal_cond = data_vehicles[data_vehicles['condition'] == 'salvage']
+if good:
+    cond_hist = px.histogram(good_cond, x='odometer', y='price')
+    st.plotly_chart(cond_hist, use_container_width=True)
+elif like_new:
+    cond_hist = px.histogram(l_n_cond, x='odometer', y='price')
+    st.plotly_chart(cond_hist, use_container_width=True)
+elif excellent:
+    cond_hist = px.histogram(ex_cond, x='odometer', y='price')
+    st.plotly_chart(cond_hist, use_container_width=True)
+elif new:
+    cond_hist = px.histogram(new_cond, x='odometer', y='price')
+    st.plotly_chart(cond_hist, use_container_width=True)
+elif fair:
+    cond_hist = px.histogram(fair_cond, x='odometer', y='price')
+    st.plotly_chart(cond_hist, use_container_width=True)
+elif salvage:
+    cond_hist = px.histogram(sal_cond, x='odometer', y='price')
+    st.plotly_chart(cond_hist, use_container_width=True)
+st.header("This web page is not complete...")
